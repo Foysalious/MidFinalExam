7deb1a9a96888301bf1377d7b7d10158c4954794
rajon <mrrajon76@gmail.com>
2020-08-16 13:53:01 +0600

    user model

--- models/user-model.js
+++ models/user-model.js
@@ -11,3 +11,46 @@ module.exports= {
 			}
 		});
 	},
+	validate: function(user, callback){
+		var sql ="SELECT * FROM user where username=? and password=?";
+
+		db.getResults(sql, [user.username, user.password], function(results){
+			if(results.length > 0){
+				callback(true);
+			}else{
+				callback(false);
+			}
+		});
+	},
+	getByUsername : function(username, callback){
+		var sql = "select * from user where username=?";
+		db.getResults(sql, [username], function(results){
+			if(results.length > 0){
+				callback(results[0]);
+			}else{
+				callback(null);
+			}
+		});
+	},
+	update : function(user, callback){
+		var sql = "update user set employee_name=?, contact_no=?, password=? where username=?";
+		db.execute(sql, [user.employee_name, user.contact_no, user.password, user.username], function(status){
+			if(status){
+				callback(true);
+			}else{
+				callback(false);
+			}
+		});
+	},
+	delete : function(user, callback){
+
+		var sql = "Delete from user where username=?";
+		db.execute(sql, [user.username], function(status){
+			if(status){
+				callback(true);
+			}else{
+				callback(false);
+			}
+		});
+	}
+}
